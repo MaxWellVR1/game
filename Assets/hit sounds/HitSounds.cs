4c9882875795f6638af7c456240705db75c1e6b9
Dragon <stevyn034@gmail.com>
2023-05-31 08:40:21 -0600

    Updated HitSounds.cs, added hitsounds to ramps, added missing floor and walls

--- Assets/hit sounds/HitSounds.cs
+++ Assets/hit sounds/HitSounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HitSounds : MonoBehaviour
 {
-    public AudioSource Hitsound;
+    [SerializeField] private AudioSource Hitsound;
 
     void OnTriggerEnter()
     {
